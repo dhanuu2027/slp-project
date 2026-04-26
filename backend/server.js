f4eaa7b107902919c7eb9fb723a9f634774d70bc
Dhanu <dhanuu2027@gmail.com>
2026-04-26 17:54:22 +0530

    fix CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const progressRoutes = require('./routes/progress');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://slp-project.vercel.app'], credentials: true }));
 app.use(express.json());
 
 // Routes
