c489ec44049154d1f9c7e18d7a8ec03de4be0078
Dhanu <dhanuu2027@gmail.com>
2026-04-26 17:19:13 +0530

    add backend URL

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
-
+axios.defaults.baseURL = 'https://slp-project.onrender.com';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
